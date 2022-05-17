80bbebbc816442c98a9351d37a18863b8bf77c45
Sunalselim <92879633+Sunalselim@users.noreply.github.com>
2022-05-17 18:37:36 +0200

    textfield

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -1,12 +1,15 @@
-
+import React from 'react';
 import './App.css';
+import TextField from '@mui/material/TextField';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-       
+      <TextField id="standard-basic" label="Kundennummer" variant="standard" />
+
+   
         
       </header>
     </div>
